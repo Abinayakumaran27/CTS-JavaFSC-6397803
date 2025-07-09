19469796b4a060ea826795d637fe867bed549a91
Abinaya K <166111788+Abinayakumaran27@users.noreply.github.com>
2025-07-09 19:10:01 +0530

    Create OrmLearnApplication.java

---
+++ Week3/Spring Data JPA - Quick Example /OrmLearnApplication.java
@@ -0,0 +1,13 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmLearnApplication.class, args);
+        LOGGER.info("Inside main");
+    }
+}
