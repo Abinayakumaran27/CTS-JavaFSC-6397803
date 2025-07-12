bb675db5e02df68cd3c03d25816a75aafdc4bd34
Abinaya K <166111788+Abinayakumaran27@users.noreply.github.com>
2025-07-12 20:10:11 +0530

    Create SpringLearnApplication.java

---
+++ Week4/CreateaSpringWebProjectusingMaven /SpringLearnApplication.java
@@ -0,0 +1,7 @@
+@SpringBootApplication
+public class SpringLearnApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearnApplication.class, args);
+        System.out.println("SpringLearnApplication started...");
+    }
+}
